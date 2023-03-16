4f0dc9ac76c6774a05e9ebcc97f76463e906c50a
Victor Lahr <lahr.victor@gmail.com>
2023-03-16 17:28:24 -0300

    feat: implement folder structure and add index.html, favicon.ico and CSS global styles

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import GlobalStyle from "../assets/styles/GlobalStyle";
+import ResetStyle from "../assets/styles/ResetStyle";
+
+export function App() {
+  return (
+      <>
+          <ResetStyle />
+          <GlobalStyle />
+      </>
+  );
+}
