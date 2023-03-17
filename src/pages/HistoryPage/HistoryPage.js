b72ba6e9cca1ed6aca21dc30f0d93bc584b676d6
Victor Lahr <lahr.victor@gmail.com>
2023-03-17 19:05:57 -0300

    feat: implement static history page

--- src/pages/HistoryPage/HistoryPage.js
+++ src/pages/HistoryPage/HistoryPage.js
@@ -1,11 +1,39 @@
 import { Header } from "../../components/Header";
 import { Menu } from "../../components/Menu";
 
+import styled from "styled-components";
+
 export function HistoryPage() {
     return (
         <>
-            <Header/>
+            <Header />
+                <Container>
+                    <h2>Histórico</h2>
+                    <p>Em breve você poderá ver o histórico dos seus hábitos aqui!</p>
+                </Container>
             <Menu />
         </>
     );
 }
+
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    padding: 100px 15px;
+    background-color: #E5E5E5;
+
+    h2 {
+        font-weight: 400;
+        font-size: 22px;
+        line-height: 28px;
+        color: #126BA5;
+    }
+
+    p {
+        font-weight: 400;
+        font-size: 18px;
+        line-height: 22px;
+        color: #666666;
+        margin-top: 18px;
+    }
+`
