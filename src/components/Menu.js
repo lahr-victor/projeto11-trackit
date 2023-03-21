9e71495bcc5fb959896bdf025a60557115f6d004
Victor Lahr <lahr.victor@gmail.com>
2023-03-21 11:23:14 -0300

    fix: change menu items spacing to work on larger screens

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -42,9 +42,9 @@ export function Menu() {
 const Container = styled.div`
     width: 100%;
     height: 70px;
-    padding: 20px 30px;
+    padding: 20px 0px;
     display: flex;
-    justify-content: space-between;
+    justify-content: space-evenly;
     align-items: center;
     background-color: #FFFFFF;
     position: fixed;
